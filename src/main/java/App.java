1eb4ec9593c21ccf7de82b45bc8e9499dfce629c
Eremey Lazar <elazar1926@gmail.com>
2023-11-13 19:22:47 +0300

    done. test doesn't work

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat beenCat = (Cat) applicationContext.getBean("cat");
+        Cat beenCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Hello Words equal - " + (bean == bean2));
+        System.out.println("Cats equal - " + (beenCat == beenCat2));
+
     }
 }
